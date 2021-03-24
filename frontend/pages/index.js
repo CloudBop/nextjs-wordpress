adf0182ee51a481587487216198818a7c1134ed2
Cloudbop <boppincolin@gmail.com>
2021-03-24 00:39:26 +0000

    added query to static props on index

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,26 @@
 // import Head from 'next/head'
-
-export default function Index() {
+import client from "../src/apollo/client";
+import { GET_MENUS } from "../src/queries/get-menus";
+export default function Index({ menus }) {
+  console.log(`menus`, menus);
   return (
     <div>
-      <h1>Index</h1>
+      <h1 className={"text-lg leading-6 font-medium text-gray-900"}>Index</h1>
     </div>
   );
+}
+
+export async function getStaticProps(context) {
+  const { data, loading, networkStatus } = await client.query({
+    query: GET_MENUS
+  });
+
+  return {
+    props: {
+      menus: {
+        headerMenus: data?.headerMenus?.edges,
+        footerMenus: data?.footerMenus?.edges
+      }
+    }
+  };
 }
