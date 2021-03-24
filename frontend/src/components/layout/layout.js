4df4771077d46f54dc4c8b44419a0d528f8c5ba4
Cloudbop <boppincolin@gmail.com>
2021-03-24 22:58:24 +0000

    +seo component to layout

--- frontend/src/components/layout/layout.js
+++ frontend/src/components/layout/layout.js
@@ -2,15 +2,47 @@ import React from "react";
 import Footer from "./footer";
 import Header from "./header";
 import Head from "next/head";
+import Seo from "../seo";
+import { isEmpty } from "lodash";
+import { sanitize } from "../../utils/misc";
+
 function Layout({ data, children }) {
+  const {
+    page,
+    //TODO destructure all this
+    // post,
+    // posts
+    header,
+    footer,
+    menus
+  } = data || {};
+  // If it does not have either post or page.
+  if (
+    isEmpty(page)
+    // && isEmpty(post) && isEmpty(posts)
+  ) {
+    return null;
+  }
+
   return (
     <div>
+      <Seo seo={page?.seo} uri={page.uri} />
       <Head>
-        <link rel="shortcut icon" href={data?.header?.favicon || ""} />
+        <link rel="shortcut icon" href={header?.favicon || ""} />
+        {page?.seo?.schemaDetails ? (
+          <script
+            type="application/ld+json"
+            className="yoast-schema-graph"
+            key="yoastSchema"
+            dangerouslySetInnerHTML={{
+              __html: sanitize(page?.seo?.schemaDetails)
+            }}
+          />
+        ) : null}
       </Head>
-      <Header header={data?.header} headerMenus={data?.menus?.headerMenus} />
+      <Header header={header} headerMenus={menus?.headerMenus} />
       <div className={"h-almost-screen"}>{children}</div>
-      <Footer footer={data?.footer} footerMenus={data?.menus?.footerMenus} />
+      <Footer footer={footer} footerMenus={menus?.footerMenus} />
     </div>
   );
 }
