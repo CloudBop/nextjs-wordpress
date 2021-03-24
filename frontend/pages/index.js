786cfbeba64ed13fc3ef1f30bf9f2e3609c72dd6
Cloudbop <boppincolin@gmail.com>
2021-03-24 17:26:40 +0000

    rmvd comment

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,7 +3,6 @@ import client from "../src/apollo/client";
 import Layout from "../src/components/layout/layout";
 import { GET_MENUS } from "../src/queries/get-menus";
 export default function Index({ data }) {
-  console.log(`data`, data);
   return (
     <Layout data={data}>
       <h1 className={"text-lg leading-6 font-medium text-gray-900"}>Index</h1>
