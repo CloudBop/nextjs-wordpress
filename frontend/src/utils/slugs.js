cb9b0e0b1d9fa929234732977797c57397fc0ecf
Cloudbop <boppincolin@gmail.com>
2021-03-25 00:43:36 +0000

    + handleRedirectsAndReturnData()

--- frontend/src/utils/slugs.js
+++ frontend/src/utils/slugs.js
@@ -1,5 +1,37 @@
+import { isEmpty } from "lodash";
+
 export const isCustomPageUri = uri => {
   const pagesToExclude = ["/", "/blog/", "/news/"];
 
   return pagesToExclude.includes(uri);
+};
+
+//
+// if data doesn't exist in wpgraphql
+//
+export const handleRedirectsAndReturnData = (
+  defaultProps,
+  data,
+  errors,
+  field
+) => {
+  if (isEmpty(data)) {
+    return {
+      redirect: {
+        //  https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/503 - service unavailable
+        destination: "/503",
+        statusCode: 301
+      }
+    };
+  }
+
+  // if field doesnt exist on data
+  if (field && isEmpty(data?.[field])) {
+    return {
+      // returns the default 404 page with a status code of 404
+      notFound: true
+    };
+  }
+
+  return defaultProps;
 };
