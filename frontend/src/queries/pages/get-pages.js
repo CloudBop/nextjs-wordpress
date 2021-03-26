89a28e2b1f30b3191f268f3ba64430a0276cf0e8
Cloudbop <boppincolin@gmail.com>
2021-03-26 21:53:58 +0000

    FIX: gql syntax error

--- frontend/src/queries/pages/get-pages.js
+++ frontend/src/queries/pages/get-pages.js
@@ -8,7 +8,7 @@ import { PRE_RENDER_PAGES_COUNT } from "../../utils/slugs";
 export const GET_PAGES_URI = gql`
   query GET_PAGES_URI {
     # limit number of pre-rendered pages
-    pages: pages(first: ${PRE_RENDER_PAGES_COUNT}}) {
+    pages: pages(first: ${PRE_RENDER_PAGES_COUNT}) {
       nodes {
         id
         uri
