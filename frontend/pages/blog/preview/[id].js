84d5afa6487d5a1ebefe2c1c8374b17a15dac918
Cloudbop <boppincolin@gmail.com>
2021-03-26 21:59:01 +0000

    +auth+preview blog/post

--- frontend/pages/blog/preview/[id].js
+++ frontend/pages/blog/preview/[id].js
@@ -1,5 +1,57 @@
-function PostPreview() {
-  return <div>post preview</div>;
+import client from "../../../src/apollo/client";
+import { GET_PAGE_BY_ID } from "../../../src/queries/pages/get-page";
+import Layout from "../../../src/components/layout/layout";
+import { handleRedirectsAndReturnData } from "../../../src/utils/slugs";
+import { getAuthToken } from "../../../src/utils/cookies";
+import { getLoginPreviewRedirectUrl } from "../../../src/utils/redirects";
+import { GET_POST_BY_ID } from "../../../src/queries/posts/get-post";
+import { sanitize } from "../../../src/utils/misc";
+
+const PostPreview = ({ data }) => {
+  return (
+    <Layout data={data} isPost>
+      <div>post preview</div>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: sanitize(data?.post?.content ?? {})
+        }}
+      />
+    </Layout>
+  );
+};
+
+export async function getServerSideProps(context) {
+  const authToken = getAuthToken(context.req);
+
+  const { params } = context || {};
+  const { data, errors } = await client.query({
+    query: GET_POST_BY_ID,
+    variables: {
+      id: Number(params?.id ?? "")
+    },
+    context: {
+      headers: {
+        authorization: authToken ? `Bearer ${authToken}` : ""
+      }
+    }
+  });
+
+  const defaultProps = {
+    props: {
+      data: data || {}
+    }
+  };
+
+  const loginRedirectURL = getLoginPreviewRedirectUrl("post", params?.id ?? "");
+
+  return handleRedirectsAndReturnData(
+    defaultProps,
+    data,
+    errors,
+    "post",
+    true,
+    loginRedirectURL
+  );
 }
 
 export default PostPreview;
