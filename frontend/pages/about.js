67b024861e5202fa4d48007aa6f8dcf1ce2efcc5
Cloudbop <boppincolin@gmail.com>
2021-03-27 02:50:44 +0000

    +mkup see-diff

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function About() {
-  return <div>about page</div>;
+  return (
+    <>
+      <header>
+        <h1>Custom Header Template</h1>
+      </header>
+      <div>about page</div>
+      <footer>
+        <h6>Custom Footer Template</h6>
+      </footer>
+    </>
+  );
 }
 
 export default About;
