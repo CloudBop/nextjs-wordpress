d877f9e279fc38f97c77f120a6cadc3a60d112ce
Cloudbop <boppincolin@gmail.com>
2021-04-02 15:41:08 +0100

    setup external domains for NextJs  Imgs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,12 @@
+/**
+ * need to re-run development/build
+ */
+
 const path = require("path");
-// const allowedImageWordPressDomain = new URL(process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL).hostname
+//
+const allowedImageWordPressDomain = new URL(
+  process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL
+).hostname;
 
 module.exports = {
   trailingSlash: true,
@@ -13,13 +20,13 @@ module.exports = {
   },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")]
-  }
+  },
   /**
    * We specify which domains are allowed to be optimized.
    * This is needed to ensure that external urls can't be abused.
    * @see https://nextjs.org/docs/basic-features/image-optimization#domains
    */
-  // images: {
-  // 	domains: [ allowedImageWordPressDomain, 'via.placeholder.com' ],
-  // },
+  images: {
+    domains: [allowedImageWordPressDomain, "via.placeholder.com"]
+  }
 };
